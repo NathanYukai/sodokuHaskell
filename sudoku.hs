f9b0b8aebdd6008b5d81b56417e5436359462ed4
NathanYukai <zlcghoul@hotmail.com>
2018-02-01 23:59:41 +0000

    found a working sudoku, the input one doesn't work, run into infinite loops

--- sudoku.hs
+++ sudoku.hs
@@ -3,40 +3,8 @@ import Debug.Trace
 import Data.Maybe
 import Data.List.Split
 
-
-shift :: [a] -> Int -> [a]
-shift l n = drop n l ++ take n l
-
-allRotations :: [a] -> [[a]]
-allRotations l = [shift l i | i <- [0..(length l)-1]]
-
-linearSearch :: String -> [String] -> Int -> Int
-linearSearch str [] idx = -1
-linearSearch str (s:source) idx
-  | str == s = idx
-  | otherwise = linearSearch str source (idx+1)
-
-binarySearch :: String -> [String] -> Int -> Int
-binarySearch _ [] _ = -1
-binarySearch str source ptr
-  | middleStr == str = middleIdx + ptr
-  | middleStr > str = binarySearch str leftH ptr
-  | otherwise = binarySearch str rightH (ptr+middleIdx)
-    where middleIdx = length source `div` 2
-          middleStr = source !! middleIdx
-          (leftH,rightH) = splitAt middleIdx source
-
-
 empty :: Int
-empty = 0 
-
-isSubRegionValid :: [Int] -> Bool
-isSubRegionValid ns = length ns == 9 && and [b `elem` ns | b <-[1..9]]
-
--- just check if there are duplicated
-isSubRegionPossible :: [Int] -> Bool
-isSubRegionPossible ns = length (nub filled) == (length filled)
-    where filled = filter (\a -> a `elem` [1..9]) ns
+empty = 0
 
 getRow :: [Int] -> Int -> [Int]
 getRow ns i = drop (i*9) . take (i*9+9) $ ns
@@ -51,21 +19,20 @@ getReg ns i = map (\n -> ns !! n) surroundIdx
           getSurround n = concat $ map (\s -> [s..s+2]) [n,n+9,n+18]
           surroundIdx = getSurround start
 
-type Valid = (Bool,Bool,Bool)
+isSudokuPossible :: [Int] -> Bool
+isSudokuPossible gm = not $ or [isPosDead pos | pos <- allZero]
+  where allZero = allEmptyPos gmap
+        isPosDead p = findPossibleAtPos gmap p == []
+        gmap = fillOnes gm
 
-isSudokuValid :: [Int] -> Valid
-isSudokuValid ns 
-  | (0 `elem` ns) = (False, False,False)
-  | otherwise = isSudoku_ ns isSubRegionValid
-
-isSudokuPossible :: [Int] -> Valid
-isSudokuPossible ns = isSudoku_ ns isSubRegionPossible
-
-isSudoku_ :: [Int] -> ([Int] -> Bool) -> Valid 
-isSudoku_ ns fn = (allRowVal , allColVal , allRegVal)
-    where allRowVal = and [fn (getRow ns i) | i <- [0..8] ]
-          allColVal = and [fn (getCol ns i) | i <- [0..8] ]
-          allRegVal = and [fn (getReg ns i) | i <- [0..8] ]
+fillOnes :: [Int] -> [Int]
+fillOnes gm
+  | allDecidedP == [] = gm
+  | otherwise = fillOnes $ repl gm (head allDecidedP) val
+  where allZero = allEmptyPos gm
+        isDecided p = length(findPossibleAtPos gm p) == 1
+        allDecidedP = filter isDecided allZero
+        val = head $ findPossibleAtPos gm $ head allDecidedP
 
 getMap :: String -> [Int]
 getMap str = map (\s -> read s :: Int) rawChar
@@ -81,48 +48,73 @@ allNum = [1..9]
 allEmptyPos :: [Int] -> [Int]
 allEmptyPos ns = [a | a <- [0..80] , ns !! a == 0]
 
-allValid :: Valid -> Bool
-allValid (True,True,True) = True
-allValid (_,_,_) = False
-
 getAllPossibleSubMap :: [Int] -> [[Int]]
-getAllPossibleSubMap m = [s | s <- allS m , allValid $ isSudokuPossible s]
-    where allS sm = [ repl sm pos val | pos <- allPos, val <- allNum]
+getAllPossibleSubMap m = [s | s <- allS , isSudokuPossible s]
+    where allS = [ repl m pos val | pos <- allPos, val <- (findPossibleAtPos m pos) ]
           allPos = allEmptyPos m
 
-haveSolution :: [[Int]] -> Int
-haveSolution ns = 
-    case idx of
-      (Nothing) -> -1
-      (Just i) -> i
-    where idx = elemIndex True valid
-          valid = map (allValid . isSudokuValid) ns 
+findPossibleAtPos :: [Int] -> Int -> [Int]
+findPossibleAtPos gmap pos = ((allNum \\ rowCon) \\ colCon) \\ regCon
+  where rowCon = getRow gmap $ rowIdx pos
+        colCon = getCol gmap $ colIdx pos
+        regCon = getReg gmap $ regIdx pos
+
+rowIdx :: Int -> Int
+rowIdx n = n `div` 9
+
+colIdx :: Int -> Int
+colIdx n = n `mod` 9
+
+regIdx :: Int -> Int
+regIdx n = 3 * (rowIdx n `div` 3) + (colIdx n) `div` 3
+
 
 solveSodoku :: [Int] -> [Int]
-solveSodoku map = head $ solveSodoku_Depth $ getAllPossibleSubMap map
+solveSodoku map = head $ filter (\p -> length p > 0) (solveSodoku_Depth $ getAllPossibleSubMap map)
 
 solveSodoku_Depth :: [[Int]] -> [[Int]]
-solveSodoku_Depth [] = []
+solveSodoku_Depth [] = [[]]
+solveSodoku_Depth [[]] = [[]]
 solveSodoku_Depth (n:ns)
-  | (length . filter (\p -> p==0)) n > 10 = (solveSodoku_Depth $ getAllPossibleSubMap n) ++ rest
---  | allValid (isSudokuValid n) = [n] -- ++ solveSodoku_Depth ns
+  | (length (filter (\p -> p==0) n )) > 0 = (solveSodoku_Depth $ getAllPossibleSubMap n) ++ rest
   | otherwise = [n]
     where rest = solveSodoku_Depth ns
 
+examplePuzzle :: [Int]
+examplePuzzle = [5, 3, 0,  0, 7, 0,  0, 0, 0,
+                 6, 0, 0,  1, 9, 5,  0, 0, 0,
+                 0, 9, 8,  0, 0, 0,  0, 6, 0,
+
+                 8, 0, 0,  0, 6, 0,  0, 0, 3,
+                 4, 0, 0,  8, 0, 3,  0, 0, 1,
+                 7, 0, 0,  0, 2, 0,  0, 0, 6,
+
+                 0, 6, 0,  0, 0, 0,  2, 8, 0,
+                 0, 0, 0,  4, 1, 9,  0, 0, 5,
+                 0, 0, 0,  0, 8, 0,  0, 7, 0]
+
 testDepth :: [[Int]] -> Int -> Int
-testDepth [] count = count 
-testDepth (n:ns) count 
-  | 0 `elem` n = testDepth (getAllPossibleSubMap n) (count +1) 
+testDepth [] count = count
+testDepth (n:ns) count
+  | 0 `elem` n = testDepth (getAllPossibleSubMap n) (count +1)
   | otherwise = count
 
-solveSodoku_Rec :: [[Int]] -> [Int]
-solveSodoku_Rec subMaps 
-  | solutionIdx == -1 = solveSodoku_Rec $ concat $ map getAllPossibleSubMap subMaps
-  | otherwise = subMaps !! solutionIdx
-    where solutionIdx = haveSolution subMaps
+printSudoku :: [Int] -> String
+printSudoku ns = "+++\n" ++ (concat $ insertEveryThird lines "---+---+---\n")
+  where lines = map printOneLine nns
+        nns = chunksOf 9 ns
+
+printOneLine :: [Int] -> String
+printOneLine ns = insertEveryThird strs '|' ++ ['\n']
+  where strs = concat $ map show ns
+
+insertEveryThird :: [a] -> a -> [a]
+insertEveryThird (a:b:c:d:e:f:x:y:z:[]) extra = a:b:c:extra:d:e:f:extra:x:y:z:[]
 
 main = do
     str <- getContents
     let gmap = getMap str
+    putStrLn $ show $ gmap
+    putStrLn $ show $ findPossibleAtPos gmap 8
     putStrLn $ show $ solveSodoku gmap
 
